50b0396f128ed016482cdb2d2bfc1cdf3e3d7dec
Alan Santos <alansantosmg@gmail.com>
2018-12-08 00:34:53 -0200

    design inicial das classes do projeto concluído

--- fshop2.py
+++ fshop2.py
@@ -8,6 +8,29 @@ Aplicação comentada para propósito de estudos
 """
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Definição dos itens de menu principal
 '''Alan's Loja de Roupas
 
@@ -26,6 +49,26 @@ Digite sua opção: '''
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class StockItem(object):  # (object define superclasse de maior nível)
     """ Superclasse com atributos principais de item de estoque """
     
@@ -36,23 +79,70 @@ class StockItem(object):  # (object define superclasse de maior nível)
         self.__stock_level = 0
         self.__StockItem_version = 1
 
-    """ propriedade com metodo acesso ao preco de estoque de 1 item instanciado """   
+
+    @property
+    def item_name(self): 
+        """ Retorna um valor estático guardado na propriedade (sem atributo)"""
+        return 'Stock Item' # o valor retornado será o nome do objeto da classe
+
     @property
     def price(self): 
+        """ propriedade com metodo acesso ao preco de estoque de 1 item instanciado """   
         return self.__price
 
-    """ propriedade com metodo acesso a quantidade de estoque de 1 item instanciado """      
+        
     @property
     def stock_level(self):
+        """ propriedade com metodo acesso a quantidade de estoque de 1 item instanciado """  
         return self.__stock_level
 
-    """ propriedade com metodo de checagem de versão da classe para fins de migracao """
+    
     def check_version(self):
+        """ classe na versão 1 não precisa atualizar nada"""
         pass
     
-    """ Metodo de impressao do objeto da classe """      
+         
     def __str__(self):
-        pass
+        """ Metodo de impressao do objeto da classe """ 
+        template ='''Stock Reference: {0}
+Type: {1}
+price: {2}
+Stock Level: {3}
+Color: {4}'''
+        return template.format(self.stock_ref, self.item_name, 
+                                self.price, self.stock_level, self.color)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Dress(StockItem): 
     """ Subclasse de Stock Item """
@@ -60,65 +150,182 @@ class Dress(StockItem):
         super().__init__(stock_ref, price, color) # herança do construtor da superclasse           
         self.pattern = pattern
         self.size = size
-        self.__dress_version = 1
+        self.__Dress_version = 1
+         
+    @property # esta propriedade está sobrescrevendo (overriding) a da superclasse
+    def item_name(self): 
+        """ Retorna um valor estático guardado na propriedade (sem variavel atributo)"""
+        return 'Dress' # a string retornada será o nome do objeto da classe
 
-    """ propriedade com metodo acesso a quantidade de estoque do item pants """  
-    @property
-    def stock_level(self):
-        return self.__stock_level
 
-    """ propriedade com metodo de checagem de versão da classe para fins de migracao """
     def check_version(self):
+        """ classe na versão 1 não precisa atualizar nada"""
+        super().check_version() # Precisa checar versão da superclasse, tb alem da subclasse
         pass
-
-    """ Metodo de impressao do objeto da classe """    
+        
+     
     def __str__(self):
-        pass
+        """ Metodo de impressao do objeto da classe """ 
+        #Exemplo de polimorfismo: Mescla atributos e métodos da super e subclasse         
+        stock_details = super().__str__()  #Guarda retorno do método str da superclasse
+        template ='''{0}
+Pattern: {1}
+Size: {2}'''
+        # retorna o retorno do metodo str da superclasse e de atributos da subclasse
+        return template.format(stock_details,self.pattern, self.size)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Pants(StockItem):
     """ Subclasse de StockItem """
     def __init__(self, stock_ref, price, color, pattern, length, waist, size):
         super().__init__(stock_ref, price, color) # heranca do contrutor da superclase 
-        self.__stock_level = 0
         self.pattern = pattern
         self.length = length
         self.waist = waist
         self.size = size
-
-
-    """ propriedade com metodo de acesso a quantidade de estoque do item pants """
-    @property
-    def stock_level(self): 
-        return self.__stock_level
+        self.__Pants_version = 1
     
-    """ propriedade com metodo de checagem de versão da instancia da classe para fins de migracao """
-    def check_version(self): 
+    @property # esta propriedade está sobrescrevendo (overriding) a da superclasse
+    def item_name(self): 
+        """ Retorna um valor estático guardado na propriedade (sem variavel atributo)"""
+        return 'Pants' # a string retornada será o nome do objeto da classe
+    
+    def check_version(self):
+        """ classe na versão 1 não precisa atualizar nada"""
+        super().check_version() # Precisa checar versão da superclasse, tb alem da subclasse
         pass
 
-    """ Metodo de impressao da instancia  da classe """  
+      
     def __str__(self): 
-        pass
+        """ Metodo de impressao da instancia  da classe """
+        #Exemplo de polimorfismo: Mescla atributos e métodos da super e subclasse         
+        stock_details = super().__str__()  #Guarda retorno do método str da superclasse
+        template ='''{0}
+Pattern: {1}
+Length: {2}
+Waist: {3}
+Size: {4}'''
+        # retorna o retorno do metodo str da superclasse e de atributos da subclasse
+        return template.format(stock_details,self.pattern, self.length, self.waist, self.size)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Jeans(Pants):
     """ Subclasse de StockItem """
     def __init__(self, stock_ref, price, color, pattern, length, size, waist, style):
-        super().__init__(stock_ref, price, color, pattern, length, size, waist) # heranca do contrutor da superclase 
-        self.__stock_level = 0
+        super().__init__(stock_ref, price, color,pattern,length,waist,size) # heranca do contrutor da superclase 
         self.style = style
-
-
-    """ propriedade com metodo de acesso a quantidade de estoque do item pants """
-    @property
-    def stock_level(self): 
-        return self.__stock_level
+        self.__Jeans_version = 1
     
-    """ propriedade com metodo de checagem de versão da instancia da classe para fins de migracao """
+    @property # esta propriedade está sobrescrevendo (overriding) a da superclasse
+    def item_name(self): 
+        """ Retorna um valor estático guardado na propriedade (sem variavel atributo)"""
+        return 'Jeans' # a string retornada será o nome do objeto da classe
+    
     def check_version(self): 
+        """ classe na versão 1 não precisa atualizar nada"""
+        super().check_version() # Precisa checar versão da superclasse, tb alem da subclasse
         pass
 
-    """ Metodo de impressao da instancia  da classe """  
+    
     def __str__(self): 
+        """ Metodo de impressao da instancia  da classe """  
+        #Exemplo de polimorfismo: Mescla atributos e métodos da super e subclasse         
+        stock_details = super().__str__()  #Guarda retorno do método str da superclasse
+        template ='''{0}
+Style: {1}
+'''
+        # retorna o retorno do metodo str da superclasse e de atributos da subclasse
+        return template.format(stock_details,self.style)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Blouses(StockItem):
+    def __init__(self, stock_ref, price, color, size, pattern, style):
+        super().__init__(stock_ref, price, color)
+        self.pattern = pattern
+        self.style = style
+        self.size = size
+        self.__Blouses_version = 1
+
+    @property # esta propriedade está sobrescrevendo (overriding) a da superclasse
+    def item_name(self): 
+        """ Retorna um valor estático guardado na propriedade (sem variavel atributo)"""
+        return 'Blouse' # a string retornada será o nome do objeto da classe
+
+    def check_version(self):
+        """" classe na versão 1 não precisa atualizar nada"""
+        super().check_version() # Precisa checar versão da superclasse, tb alem da subclasse
         pass
+    
+    def __str__(self):
+        """ Metodo de impressao da instancia  da classe """ 
+        #Exemplo de polimorfismo: Mescla atributos e métodos da super e subclasse         
+        stock_details = super().__str__()  #Guarda retorno do método str da superclasse
+        template ='''{0}
+Pattern: {1}
+Style {2}
+Size: {3}'''
+        # retorna o retorno do metodo str da superclasse e de atributos da subclasse
+        return template.format(stock_details, self.pattern, self.style, self.size)
 
 
 
@@ -131,13 +338,97 @@ class Jeans(Pants):
 
 
 
-# testes de classe
 
-x = Dress(stock_ref ='d0001', price=10, color='red', pattern='circulos', size='m')
-print(x.price)
 
-y = Pants(stock_ref ='d0001', price=10, color='red', pattern='circulos', waist='m', length=10, size='m')
-print(y.price)
 
-z = Jeans(stock_ref ='d0001', price=10, color='red', pattern='circulos', waist='m', length=10, style='cut', size='m')
-print(z.price)
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Hats(StockItem): # subclasse de stockitem
+    def __init__(self, stock_ref, price, color, size):  # construtor       
+        super().__init__(stock_ref, price, color) # herda construtor de StockItem
+        self.size = size
+        self.__hats_version = 1
+
+    @property # esta propriedade está sobrescrevendo (overriding) a da superclasse
+    def item_name(self): 
+        """ Retorna um valor estático guardado na propriedade (sem variavel atributo)"""
+        return 'Hat' # a string retornada será o nome do objeto da classe   
+
+    def check_version(self):
+        """ classe na versão 1 não precisa atualizar nada"""
+        super().check_version() # Precisa checar versão da superclasse, tb alem da subclasse
+        pass
+    
+    def __str_(self):
+        """ Metodo de impressao da instancia  da classe """  
+        #Exemplo de polimorfismo: Mescla atributos e métodos da super e subclasse         
+        stock_details = super().__str__()  #Guarda retorno do método str da superclasse
+        template ='''{0}
+Size: {1}'''
+        # retorna o retorno do metodo str da superclasse e de atributos da subclasse
+        return template.format(stock_details, self.size)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+v = Dress(stock_ref ='d0001', price=10, color='red', pattern='circulos', size='m')
+
+w = Pants(stock_ref ='d0001', price=10, color='red', pattern='circulos', waist='m', length=10, size='m')
+
+x = Jeans(stock_ref ='d0001', price=10, color='red', pattern='circulos', waist='m', length=10, style='cut', size='m')
+
+y = Blouses(stock_ref ='d0001', price=120, color='red', pattern='circulos', style='m', size='m')
+
+z = Hats(stock_ref ='d0001', price=10, color='red', size='m')
+
+
+
+
+test_print ='''Teste de impressão de classes)
+
+{0}
+
+{1}
+
+{2}
+
+{3}
+
+{4}'''
+
+print(test_print.format(v,w,x,y,z))
